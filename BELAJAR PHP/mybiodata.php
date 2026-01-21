237bb8a401d686c3a6d242151ee23e57f978ab65
Dinara <dinaraalifah41@gmail.com>
2026-01-21 10:39:48 +0700

    belajar php

---
+++ BELAJAR PHP/mybiodata.php
@@ -0,0 +1,18 @@
+<?php
+     $nama   = "Dinara Alifah Istiqomah";
+     $ttl    = "Bandung, 17 Oktober 2008";
+     $umur   = 17;
+     $kelas  = "XI TJKT 1";
+     $alamat = "Kp Cikieum RT 04 RW 06 Ds. Mangunjaya Kec. Arjasari Kab. Bandung";
+     $jk     = "Perempuan";
+     $agama  = "Islam";
+
+     echo "<h2>My Biodata</h2>";
+     echo "Nama : $nama <br>";
+     echo "Tempat Tanggal Lahir : $ttl <br>";
+     echo "Umur : $umur <br>";
+     echo "Kelas : $kelas <br>";
+     echo "Alamat : $alamat <br>";
+     echo "Jenis Kelamin : $jk <br>";
+     echo "Agama : $agama <br>";
+     ?>
