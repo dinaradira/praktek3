645e4f64a3c25f85229fd037259b93a50b68614d
Dinara <dinaraalifah41@gmail.com>
2026-05-20 08:03:28 +0700

    up

---
+++ TEATER_LOGIN/cek.php
@@ -0,0 +1,13 @@
+<?php
+include 'koneksi.php';
+
+$q = mysqli_query($conn,"SHOW TABLES");
+
+if(!$q){
+ die(mysqli_error($conn));
+}
+
+while($row=mysqli_fetch_array($q)){
+    echo $row[0]."<br>";
+}
+?>
